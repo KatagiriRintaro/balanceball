7bc6f327c49ef72a80989136f9950767402d5469
KatagiriRintaro <130040566+KatagiriRintaro@users.noreply.github.com>
2024-10-08 19:27:48 +0900

    テスト

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,6 @@
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
-<<<<<<< HEAD
 from predictor import predictor
-=======
-import predictor 
->>>>>>> c77b0830baa7108ec8cbb14edce9792e4a122857
 
 app = Flask(__name__)
 # CORS(app)
@@ -18,17 +14,12 @@ def predict_balance_ball_training():
         if not data:
             return jsonify({"message": "データが空です"}), 400
 
-<<<<<<< HEAD
         # print(f"Received data: {data}")
         print(type(data))
-=======
-        print(f"Received data: {data}")
->>>>>>> c77b0830baa7108ec8cbb14edce9792e4a122857
         print(f'Data Length: {len(data)}')
         
         # ここでデータ解析を実行
         # predictor.py内の関数などでデータを処理することができます
-<<<<<<< HEAD
         result = predictor(data, 100, 3)
         print(type(result))
         result_json = result.to_json(orient = "records", force_ascii=False)
@@ -36,21 +27,12 @@ def predict_balance_ball_training():
         # 解析結果をJSON形式で返す
         return jsonify({"message": "データ受信成功", "data": result_json}), 200
         # return jsonify({"message": "データ受信成功"}), 200
-=======
-        result = predictor.analyze_data(data, 100, 3)
-        print(f'CCC {result}')
-        # result_json = result.to_json(orient = "records", force_ascii=False)
-        
-        # 解析結果をJSON形式で返す
-        # return jsonify({"message": "データ受信成功", "data": result_json}), 200
-        return jsonify({"message": "データ受信成功"}), 200
->>>>>>> c77b0830baa7108ec8cbb14edce9792e4a122857
     except Exception as e:
         print(f"Error: {e}")
         return jsonify({"message": "データ受信失敗"}), 400
     
 
 if __name__ == "__main__":
-    app.debug = True
-    # app.run(host='192.168.10.102', port=5000)
-    app.run(host='172.16.4.31', port=5000)
+        app.debug = True
+        # app.run(host='192.168.10.102', port=5000)
+        app.run(host='172.16.4.31', port=5000)
